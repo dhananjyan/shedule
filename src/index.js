0cc7902b71567cb7a6e2732f6bc44f2c8563f9a3
Dhananjayan625 <dhananjayan625@gmail.com>
2020-08-06 12:27:10 +0530

    'redirec

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { DataProvider } from './context/DataContext'
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect } from 'react-router-dom'
 
-const httpLink = new HttpLink({ uri: 'https://firstapis.herokuapp.com/graphql' })
+const httpLink = new HttpLink({ uri: 'http://localhost:5000/graphql' })
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
@@ -18,7 +18,7 @@ ReactDOM.render(
       <Router>
         <App />
         <h1>
-          Welcome
+          <Redirect to="/login">login</Redirect>
         </h1>
       </Router>
     </DataProvider>
